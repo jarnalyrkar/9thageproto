65bf706e4f85bd07bced62a14aa18f3a85ad48ae
jarnalyrkar <joachimsolstrom@gmail.com>
2018-07-13 23:38:37 +0200

    First draft

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include($_SERVER["DOCUMENT_ROOT"] . "/9thageproto/partials/_header.php");
-  include($_SERVER["DOCUMENT_ROOT"] . "/9thageproto/partials/_universe.php");
-  include($_SERVER["DOCUMENT_ROOT"] . "/9thageproto/partials/_game.php");
-  include($_SERVER["DOCUMENT_ROOT"] . "/9thageproto/partials/_thescroll.php");
-  include($_SERVER["DOCUMENT_ROOT"] . "/9thageproto/partials/_community.php");
-  include($_SERVER["DOCUMENT_ROOT"] . "/9thageproto/partials/_footer.php");
-?>
